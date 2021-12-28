7e46c644549975542f759f11af4f68cda3a9fccb
Eric Freitas <efreitos@gmail.com>
2021-12-28 17:38:11 -0600

    Initial Commit

---
+++ HitTestApp/HitTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitTestAppApp.swift
+//  HitTestApp
+//
+//  Created by Eric Freitas on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct HitTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
